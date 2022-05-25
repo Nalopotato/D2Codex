255abc74b5467bc3656e7337d26d5440dd83e8bc
Michael Grammer <7441544+Nalopotato@users.noreply.github.com>
2022-05-25 11:10:31 -0400

    Re-added and configured the startup files

--- D2Codex/Program.cs
+++ D2Codex/Program.cs
@@ -2,6 +2,20 @@
 {
     public class Program
     {
-        static void Main() { }
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+            //using var db = new D2ItemsContext();
+
+            //var runes = db.Runes.First();
+
+            //Console.WriteLine("Runes: " + runes.Name);
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
     }
 }
---
+++ D2Codex/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Serialization;
+
+namespace D2Codex
+{
+    public class Startup
+    {
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(c => c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+            services.AddControllersWithViews().AddNewtonsoftJson(
+                options => options.SerializerSettings.ReferenceLoopHandling=Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+                    .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
